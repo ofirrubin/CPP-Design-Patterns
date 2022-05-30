2c3e810a57c9bbb03e9142b780f715f308e3aee1
ofirrubin <ofirrubin02@gmail.com>
2022-05-30 19:13:41 +0300

    Initial Commit

---
+++ Ex6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ex6
+//
+//  Created by Ofir Rubin on 30/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
